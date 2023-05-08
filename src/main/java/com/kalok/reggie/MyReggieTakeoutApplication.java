f2fe2bac5dd7a872b8d9760438f4d886855d5a00
paullok999 <paulvon369@Outlook.com>
2023-05-08 23:27:53 +0800

    add sql file

--- src/main/java/com/kalok/reggie/MyReggieTakeoutApplication.java
+++ src/main/java/com/kalok/reggie/MyReggieTakeoutApplication.java
@@ -1,12 +1,15 @@
 package com.kalok.reggie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MyReggieTakeoutApplication {
 
     public static void main(String[] args) {
+        log.info("my-reggie-takeout 正在启动");
         SpringApplication.run(MyReggieTakeoutApplication.class, args);
     }
 
--- src/main/java/com/kalok/reggie/config/WebMvcConfig.java
+++ src/main/java/com/kalok/reggie/config/WebMvcConfig.java
@@ -1,2 +1,18 @@
-package com.kalok.reggie.config;public class WebMvcConfig {
+package com.kalok.reggie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 添加静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
 }
